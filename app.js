7681bcb3121454ba5e2c710d7cfd77463c3929c1
Bogdan Negura <id.bogdann@yahoo.com>
2019-08-18 20:40:04 +0100

    fix typo and create the functionality on js by creating the ui vars and using math create const wich its used on iteration calc and then display an error just in case it's nothing typed or a wrong caracter

---
+++ app.js
@@ -0,0 +1,61 @@
+//  Listen for Submit
+document.getElementById('loan-form').addEventListener('submit', countingResults);
+
+// Counting Results
+function countingResults(e){
+    console.log('Counting...');
+
+    // UI Vars
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    const principal = parseFloat(amount.value);
+    const calculateInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatePayments = parseFloat(years.value) * 12;
+
+    // calculate montly payment
+    const x = Math.pow(1 + calculateInterest, calculatePayments);
+    const monthly = (principal*x*calculateInterest)/(x-1);
+
+    if(isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatePayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatePayments)-principal).toFixed(2);
+    } else {
+        showError('please check your number')
+    }
+
+    e.preventDefault();
+}
+
+// Show Error
+function showError(error){
+    // Create a div
+    const errorDiv = document.createElement('div');
+
+    // Get Elements
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    // Add class
+    errorDiv.className = 'alert alert-danger';
+
+    // create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // insert error above heading
+    card.insertBefore(errorDiv, heading);
+
+    // Clear error after 3 seconds
+    setTimeout(clearError, 2000);
+
+    // Clear error
+    function clearError(){
+        document.querySelector('.alert').remove();
+    }
+}
+
